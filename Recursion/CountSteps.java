b4898bf64a3bebfe41447a8349cdecf3b880fc30
bisht2011 <51433951+bisht2011@users.noreply.github.com>
2022-06-25 09:51:24 +0530

    Add files via upload

---
+++ Recursion/CountSteps.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountSteps {
+    public static void main(String[] args) {
+int c=0;
+        System.out.println("enter the number");
+        int num = new Scanner(System.in).nextInt();
+        System.out.println("numbers of steps: " + count(num,c));
+    }
+
+    static int count(int num,int c) {
+        if(num==0)
+            return c;
+        int rem=num%10;
+        if(rem%2==0){
+            return (count(num/2,c+1));
+        }
+        else {
+            return (count(num - 1, c + 1));
+        }
+    }
+}
