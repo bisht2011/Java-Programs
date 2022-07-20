5e051aeee7bcd9b40888cd6b138c73700f4cf7ac
bisht2011 <51433951+bisht2011@users.noreply.github.com>
2022-07-20 10:28:03 +0530

    leetcode

---
+++ leetcode/LetterCombinationPhone.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class LetterCombinationPhone {
+    public static void main(String[] args) {
+        String s="23";
+        System.out.println(ansi(s));
+
+    }
+
+     static List<String> ansi(String s) {
+
+         ArrayList<String>result=new ArrayList<>();
+         if(s.isEmpty()){
+             return result;
+         }
+         HashMap<Integer,String>map=new HashMap<>();
+         map.put(2,"abc");
+         map.put(3,"def");
+         map.put(4,"ghi");
+         map.put(5,"jkl");
+         map.put(6,"mno");
+         map.put(7,"pqrs");
+         map.put(8,"tuv");
+         map.put(9,"wxyz");
+
+         helper("",s,map,result);
+         return result;
+     }
+
+    private static void helper(String s, String s1, HashMap<Integer, String> map, ArrayList<String> result) {
+        if(s1.isEmpty()){
+            result.add(s);
+            return;
+        }
+        int digit=Character.getNumericValue(s1.charAt(0));
+        for(int i=0;i<map.get(digit).length();i++){
+            helper(s+map.get(digit).charAt(i),s1.substring(1),map,result);
+        }
+    }
+}
