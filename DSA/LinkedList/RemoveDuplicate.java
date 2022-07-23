25d6d87a19983accea6e2a5561aa59e6553d9218
bisht2011 <51433951+bisht2011@users.noreply.github.com>
2022-07-23 23:26:07 +0530

    LinkedList

---
+++ DSA/LinkedList/RemoveDuplicate.java
@@ -0,0 +1,15 @@
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+
+    }
+//    if(head==null || head.next==null) return head;
+//    ListNode temp=head;
+//        while(temp!=null && temp.next!=null){
+//        int v1=temp.val,v2=temp.next.val;
+//        if(v1==v2)
+//        temp.next=temp.next.next;
+//        else
+//        temp=temp.next;
+//    }
+//        return head;
+}
