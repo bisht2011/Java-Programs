78366413ab6b9faebc16bbfa8d775e9ed35a25d0
bisht2011 <51433951+bisht2011@users.noreply.github.com>
2022-07-30 00:02:03 +0530

    LinkedList

---
+++ DSA/LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,27 @@
+public class RemoveLinkedListElements {
+    public static void main(String[] args) {
+
+    }
+//    public ListNode removeElements(ListNode head, int val) {
+//        while(head!=null &&  head.val==val){
+//            head=head.next;
+//        }
+//
+//        ListNode curr=head;
+//        while(curr!=null && curr.next!=null){
+//            if(curr.next.val==val){
+//                curr.next=curr.next.next;
+//            }else{
+//                curr=curr.next;
+//            }
+//        }
+//        return head;
+//    }
+//    if(head==null)
+//            return head;                   using recursion
+//				if(head.val==val)
+//    head=removeElements(head.next,val);
+//				else
+//    head.next=removeElements(head.next,val);
+//				return head;
+}
